fa5ca5c1ff4ba45874f124efe925a3c5e2c86472
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-23 22:26:16 +0700

    Add Who Wins Header

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDice2 = 'images/dice' + randomNumber2 + '.png';
 
 document.querySelectorAll('img')[1].setAttribute('src', randomDice2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = '🚩Player 1 Wins!'
+} else {
+    document.querySelector('h1').innerHTML = 'Player 2 Wins!🚩'
+}
