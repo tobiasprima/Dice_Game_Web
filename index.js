7b9bc51d0a460f2e82d1bfe05674ac58d4a942f2
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-23 22:22:24 +0700

    Add Random Dice Funtion

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var randomNumber1 = Math.floor(Math.random() * 9) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDice = 'images/dice' + randomNumber1 + '.png';
+var randomDice1 = 'images/dice' + randomNumber1 + '.png';
 
-document.querySelectorAll('img')[0].setAttribute('src', randomDice);
+document.querySelectorAll('img')[0].setAttribute('src', randomDice1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDice2 = 'images/dice' + randomNumber2 + '.png';
+
+document.querySelectorAll('img')[1].setAttribute('src', randomDice2);
